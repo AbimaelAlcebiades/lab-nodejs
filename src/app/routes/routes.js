23cfa6bfea212b1e63cbcfa6fabc03d5a41d28d0
Abimael Alcebiades <abimaelafsilva@gmail.com>
2019-03-05 13:30:33 -0300

    adicionando form para endpoint de livros

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,23 +1,58 @@
+const db = require('../../config/database');
+const BookDao = require('../infra/book-dao');
+
+const bookDao = new BookDao(db);
+
 module.exports = (app) => {
-    app.get('/', function(req, res){
-        res.send('Home');
-    });
-    
-    app.get('/books', function(req, res){
+    app.get('/', function (req, res) {
         res.marko(
-            require('../views/books/list/list.marko'),
-            {
-                books: [
-                    {
-                        id: 1,
-                        title: 'título 1'
-                    },
-                    {
-                        id: 2,
-                        title: 'título 2'
-                    }
-                ]
-            }
+            require('../views/home/home.marko')
+        )
+    });
+
+    app.get('/books', function (req, res) {
+
+        bookDao.list()
+            .then(books => res.marko(
+                require('../views/books/list/list.marko'),
+                {
+                    books: books
+                }
+            )).catch(error => console.log(error));
+
+        bookDao.list(function (error, results) {
+            res.marko(
+                require('../views/books/list/list.marko'),
+                {
+                    books: results
+                }
+            );
+        });
+    });
+
+    app.get('/books', function (req, res) {
+
+        bookDao.list()
+            .then(books => res.marko(
+                require('../views/books/list/list.marko'),
+                {
+                    books: books
+                }
+            )).catch(error => console.log(error));
+
+        bookDao.list(function (error, results) {
+            res.marko(
+                require('../views/books/list/list.marko'),
+                {
+                    books: results
+                }
+            );
+        });
+    });
+
+    app.get('/books/form', function (req, res) {
+        res.marko(
+            require('../views/books/form/form.marko')
         );
     });
 }
