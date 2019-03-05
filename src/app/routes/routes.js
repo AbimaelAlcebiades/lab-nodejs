cfd7a2df6bd5b767082faabda989bd14aee62fe9
Abimael Alcebiades <abimaelafsilva@gmail.com>
2019-03-04 22:50:31 -0300

    Brincando com módulo de templates marko

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,10 +1,24 @@
 module.exports = (app) => {
     app.get('/', function(req, res){
-        res.end('raiz');
+        res.send('Home');
     });
     
-    app.get('/outra', function(req, res){
-        res.end('outra URL');
+    app.get('/books', function(req, res){
+        res.marko(
+            require('../views/books/list/list.marko'),
+            {
+                books: [
+                    {
+                        id: 1,
+                        title: 'título 1'
+                    },
+                    {
+                        id: 2,
+                        title: 'título 2'
+                    }
+                ]
+            }
+        );
     });
 }
 
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,5 +1,7 @@
 const routes = require('../app/routes/routes.js');
 const express = require('express');
+require('marko/node-require').install();
+require('marko/express');
 
 const app = express();
 routes(app);
