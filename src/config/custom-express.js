1004eaa7bf8a4aa7d204f962e97552c6e9cc3157
Abimael Alcebiades <abimaelafsilva@gmail.com>
2019-03-05 17:19:26 -0300

    Configurando para realizar parser das requisições

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -2,8 +2,14 @@ const routes = require('../app/routes/routes.js');
 const express = require('express');
 require('marko/node-require').install();
 require('marko/express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(
+    bodyParser.urlencoded({ extended: true })
+);
+
 routes(app);
 
 module.exports = app;
