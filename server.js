37e30f66cf4a181b46b76b86625e14c91f8e7f4f
Abimael <abimaelafsilva@gmail.com>
2019-03-08 12:38:39 -0300

    Adicionando informação de servidor

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const app = require('./src/config/custom-express');
+const hostName = 'localhost';
+const port = 3000;
 
-app.listen(3000, function(){
-    console.log('Servidor rodando');
+app.listen(port, hostName,  function(){
+    console.log(`Servidor rodando em http://${hostName}:${port}`);
 });
 
 app.get('/', function(req, res){
     res.end(
         'Página'
-    )
+    );
 });
